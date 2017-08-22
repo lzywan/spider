1b7030d235fb6eceefbff3d6f08125b353fff0e2
卢尚鹏 <lusp@ziroom.com>
2017-08-22 18:26:05 +0800

    定时任务检测代理ip可用性

--- src/main/java/com/ziroom/minsu/spider/service/impl/ProxyIpPipelineServiceImpl.java
+++ src/main/java/com/ziroom/minsu/spider/service/impl/ProxyIpPipelineServiceImpl.java
@@ -1,14 +1,9 @@
 package com.ziroom.minsu.spider.service.impl;
 
+import com.ziroom.minsu.spider.core.utils.HttpClientUtil;
 import com.ziroom.minsu.spider.domain.NetProxyIpPort;
 import com.ziroom.minsu.spider.mapper.NetProxyIpPortMapper;
 import com.ziroom.minsu.spider.service.ProxyIpPipelineService;
-import org.apache.http.HttpHost;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +12,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import us.codecraft.webmagic.ResultItems;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -60,7 +54,7 @@ public class ProxyIpPipelineServiceImpl implements ProxyIpPipelineService{
 		List<NetProxyIpPort> netIps = resultItems.get("netIps");
 		try {
 			for (NetProxyIpPort netIp : netIps) {
-				if (checkProxyIp(airbnbUrl,netIp.getProxyIp(), netIp.getProxyPort())) {
+				if (HttpClientUtil.checkProxyIp(airbnbUrl,netIp.getProxyIp(), netIp.getProxyPort())) {
 					netIp.setFid(UUID.randomUUID().toString());
 					netProxyIpPortMapper.saveNetProxyIp(netIp);
 				} else {
@@ -73,45 +67,7 @@ public class ProxyIpPipelineServiceImpl implements ProxyIpPipelineService{
 		}
 	}
 
-	/**
-	 * @description: 判断代理ip 是否可用
-	 * @author: lusp
-	 * @date: 2017/8/22 13:51
-	 * @params: URL,ip,port
-	 * @return:
-	 */
-	public static boolean checkProxyIp(String url, String ip, int port) {
-		boolean flag = false;
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		HttpGet httpGet = new HttpGet(url);
-		RequestConfig.Builder builder = RequestConfig.custom()
-				.setSocketTimeout(5000)
-				.setConnectTimeout(5000)
-				.setMaxRedirects(50)
-				.setConnectionRequestTimeout(5000);
-		HttpHost httpHost = new HttpHost(ip,port);
-		builder.setProxy(httpHost);
-		httpGet.setConfig(builder.build());
-		CloseableHttpResponse response = null;
-		try {
-			response = httpclient.execute(httpGet);
-			int status = response.getStatusLine().getStatusCode();
-			if(status>=200&&status<=300){
-				flag = true;
-			}
-		} catch (IOException e) {
-			LOGGER.info("该代理ip地址不可用,ip:{},port:{}",ip,port);
-		} finally {
-			if (response != null) {
-				try {
-					response.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return flag;
-	}
+
 
 	//测试多线程
 	@Async
