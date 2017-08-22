5658f03aa27cdb6b667febf704376a969e904596
卢尚鹏 <lusp@ziroom.com>
2017-08-22 18:29:21 +0800

    定时任务

---
+++ src/main/java/com/ziroom/minsu/spider/service/CheckProxyIpJobService.java
@@ -0,0 +1,20 @@
+package com.ziroom.minsu.spider.service;/*
+ * <P></P>
+ * <P>
+ * <PRE>
+ * <BR> 修改记录
+ * <BR>------------------------------------------
+ * <BR> 修改日期       修改人        修改内容
+ * </PRE>
+ * 
+ * @Author lusp
+ * @Date Create in 2017年08月 22日 17:12
+ * @Version 1.0
+ * @Since 1.0
+ */
+
+public interface CheckProxyIpJobService {
+
+    void checkProxyIpAvailable();
+
+}
