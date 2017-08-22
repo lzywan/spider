e2a677b436eac85cefe33dde26cf02ad77628bac
卢尚鹏 <lusp@ziroom.com>
2017-08-22 14:43:58 +0800

    代理ip增加访问Airbnb校验是否有效

--- src/main/java/com/ziroom/minsu/spider/proxyip/processor/ProxyipPipeline.java
+++ src/main/java/com/ziroom/minsu/spider/proxyip/processor/ProxyipPipeline.java
@@ -2,17 +2,23 @@ package com.ziroom.minsu.spider.proxyip.processor;
 
 import com.ziroom.minsu.spider.domain.NetProxyIpPort;
 import com.ziroom.minsu.spider.mapper.NetProxyIpPortMapper;
+import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.pipeline.Pipeline;
-import us.codecraft.webmagic.proxy.Proxy;
-import us.codecraft.webmagic.utils.ProxyUtils;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -34,9 +40,12 @@ import java.util.UUID;
  * @version 1.0
  */
 @Service
+@EnableAsync(proxyTargetClass=true)
 public class ProxyipPipeline implements Pipeline {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ProxyipPipeline.class);
+
+	private static final String airbnbUrl = "https://www.airbnbchina.cn/";
 
 	@Autowired
 	private NetProxyIpPortMapper netProxyIpPortMapper;
@@ -46,20 +55,26 @@ public class ProxyipPipeline implements Pipeline {
 	 */
 	@Override
 	public void process(ResultItems resultItems, Task task) {
-
 		List<NetProxyIpPort> netIps = resultItems.get("netIps");
-
 		LOGGER.info("Pipeline开始处理，网站={}，当前页面={}，ip数量={}", resultItems.get("siteName"), resultItems.get("url"), netIps.size());
+		asyncCheckProxyAndSave(resultItems);
+	}
 
+	/**
+	 * @description: 启用多线程去判断代理ip是否可用，如果可用进行持久化
+	 * @author: lusp
+	 * @date: 2017/8/22 13:53
+	 * @params: resultItems
+	 * @return:
+	 */
+	@Async
+	private void asyncCheckProxyAndSave(ResultItems resultItems){
+		List<NetProxyIpPort> netIps = resultItems.get("netIps");
 		try {
 			for (NetProxyIpPort netIp : netIps) {
-				
-				if (ProxyUtils.validateProxy(new Proxy(netIp.getProxyIp(), netIp.getProxyPort()))) {
-					
+				if (checkProxyIp(airbnbUrl,netIp.getProxyIp(), netIp.getProxyPort())) {
 					netIp.setFid(UUID.randomUUID().toString());
-					
 					netProxyIpPortMapper.saveNetProxyIp(netIp);
-					
 				} else {
 					LOGGER.info("Pipeline IP无效！ 网站={}，ip={}", resultItems.get("siteName"), netIp);
 				}
@@ -68,30 +83,51 @@ public class ProxyipPipeline implements Pipeline {
 		} catch (Exception e) {
 			LOGGER.info("Pipeline process异常！ 网站={}，当前页面={}，ip数量={}，e={}", resultItems.get("siteName"), resultItems.get("url"), netIps.size(), e);
 		}
-
 	}
 
+	/**
+	 * @description: 判断代理ip 是否可用
+	 * @author: lusp
+	 * @date: 2017/8/22 13:51
+	 * @params: URL,ip,port
+	 * @return:
+	 */
+	public static boolean checkProxyIp(String url, String ip, int port) {
+		boolean flag = false;
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpGet httpGet = new HttpGet(url);
+		RequestConfig.Builder builder = RequestConfig.custom()
+				.setSocketTimeout(5000)
+				.setConnectTimeout(5000)
+				.setMaxRedirects(50)
+				.setConnectionRequestTimeout(5000);
+		HttpHost httpHost = new HttpHost(ip,port);
+		builder.setProxy(httpHost);
+		httpGet.setConfig(builder.build());
+		CloseableHttpResponse response = null;
+		try {
+			response = httpclient.execute(httpGet);
+			int status = response.getStatusLine().getStatusCode();
+			if(status>=200&&status<=300){
+				flag = true;
+			}
+		} catch (IOException e) {
+			LOGGER.info("该代理ip地址不可用,ip:{},port:{}",ip,port);
+		} finally {
+			if (response != null) {
+				try {
+					response.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return flag;
+	}
 
-	public static void main(String[] args) {
-		List<Boolean> list = new ArrayList<Boolean>();
-		
-		Proxy proxy = new Proxy("113.140.25.4", 81);
-		
-		list.add(ProxyUtils.validateProxy(proxy));
-		
-		proxy = new Proxy("43.240.138.31", 8080);
-
-		list.add(ProxyUtils.validateProxy(proxy));
-		
-		proxy = new Proxy("114.239.149.230", 808);
-
-		list.add(ProxyUtils.validateProxy(proxy));
-		
-		proxy = new Proxy("115.203.64.9", 808);
-
-		list.add(ProxyUtils.validateProxy(proxy));
-
-		System.out.println(list);
+	@Async
+	public void doTask(int i) {
+		LOGGER.info("Task"+i+" started.");
 	}
 
 }
