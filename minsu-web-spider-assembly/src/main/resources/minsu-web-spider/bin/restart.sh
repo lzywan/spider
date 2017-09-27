7b1bbb8df6e6d1f75322c80b1a84ff137cf16119
zhangyl2 <zhangyl2@ziroom.com>
2017-09-27 22:43:22 +0800

    修改脚本

--- minsu-web-spider-assembly/src/main/resources/minsu-web-spider/bin/restart.sh
+++ minsu-web-spider-assembly/src/main/resources/minsu-web-spider/bin/restart.sh
@@ -1,57 +1,96 @@
 #!/bin/bash
-#
-# zy
-# 20150516
-# 重启服务脚本
+# chkconfig: - 66 01
+#v1.0#
+
 source /etc/profile
 
-export LANG=en_US.UTF-8
-export LANG_ALL=en_US.UTF-8
-item=`pwd | awk -F "/" '{print $4}' | tr -d '\r'`
-tomcat_bin=/home/www/$item/bin
-
-/usr/sbin/ntpdate ntp.ziroom.com
-touch /tmp/8081_lock.txt
-cd $tomcat_bin ;
-echo "停止服务"
-./spring-boot stop
-for i in `seq 1 10`
-do
-echo -n .
-if ! ps aux |grep $tomcat_bin |grep -vq grep ; then
-  continue
-fi
-sleep 1
-done
-if ps aux |grep $item |grep -vq grep ; then
-  ps aux |grep $item |grep -v grep  |awk '{print $2}'|xargs kill -9
-fi
-sleep 8
-
-if [ "$1" == "stop" ] ; then
-    exit
+BASE_DIR=/home/www
+PROJECT=`pwd | awk -F "/" '{print $4}' | tr -d '\r'`
+HOME_DIR=${BASE_DIR}/${PROJECT}
+CONF_DIR=${HOME_DIR}/conf
+STDOUT_DIR=${HOME_DIR}/logs
+LIB_DIR=${HOME_DIR}/lib
+WEBAPPS_DIR=${HOME_DIR}/webapps
+LIB_JARS=`ls ${LIB_DIR}|grep .jar|awk '{print "'${LIB_DIR}'/"$0}'|tr "\n" ":"`
+if [ ! -d ${STDOUT_DIR} ]; then
+	mkdir ${STDOUT_DIR}
 fi
 
-#su - ziroom -c "cd $tomcat_bin ; sh spring-boot restart"
-su -c "cd $tomcat_bin ; sh spring-boot restart"
-echo "等待启动"
-for  i in `seq 1 3`
-do
-sleep 5
-tail -n 1 ../logs/stdout.log
-done
+JAVA_CLASS=`grep '^java_class' ${CONF_DIR}/spring-boot.properties |awk -F"java_class=" '{print $2}' | tr -d '\r'`
+JAVA_OPTS=`grep '^java_opts' ${CONF_DIR}/spring-boot.properties |awk -F"java_opts=" '{print $2}' | tr -d '\r'`
+JAVA_MEM_OPTS=`grep '^java_mem_opts' ${CONF_DIR}/spring-boot.properties|awk -F"java_mem_opts=" '{print $2}' | tr -d '\r'`
 
-echo "获取进程信息"
-ps aux |grep $item|grep -v grep
-if [ $? -gt 0 ] ; then
-#   su - ziroom -c "cd $tomcat_bin ; sh spring-boot restart"
-   su -c "cd $tomcat_bin ; sh spring-boot restart"
-   sleep 20
-fi
 
-tail -n 300 ../logs/stdout.log
-echo "获取日志"
-echo  "查看端口号"
-netstat -ntlp |grep `ps aux |grep java |grep $tomcat_bin|awk '{print $2}'`
-echo "重启完成"
-rm -fv /tmp/8081_lock.txt
+START () {
+	PIDS=`ps -ef | grep java | grep "${CONF_DIR}"|grep -v grep |awk '{print $2}'`
+	if [ -n "${PIDS}" ];then
+	   echo "${PROJECT} is running!"
+	else
+	   echo "Starting the ${PROJECT} ..."
+	   echo "JAVA_OPTS: ${JAVA_OPTS}"
+	   echo "JAVA_MEM_OPTS: ${JAVA_MEM_OPTS}"
+	   echo "CONF_DIR: ${CONF_DIR}"
+	   echo "WEBAPPS_DIR: ${WEBAPPS_DIR}"
+	   echo "LIB_JARS: ${LIB_JARS}"
+	   echo "JAVA_CLASS: ${JAVA_CLASS}"
+	   echo "STDOUT_DIR: ${STDOUT_DIR}"
+	   nohup java ${JAVA_OPTS} ${JAVA_MEM_OPTS}  -classpath ${CONF_DIR}:${WEBAPPS_DIR}:${LIB_JARS}   ${JAVA_CLASS} > ${STDOUT_DIR}/stdout.log 2>&1 &
+	   sleep 10
+	PIDA=`ps -ef | grep java | grep "${CONF_DIR}"|grep -v grep |awk '{print $2}'`
+	   if [ -z "${PIDA}" ];then
+	      echo "${PROJECT} start err!"
+	   fi
+	fi
+}
+
+STOP () {
+	PIDS=`ps -ef | grep java | grep "${CONF_DIR}"|grep -v grep |awk '{print $2}'`
+	  if [ -z "${PIDS}" ];then
+	     echo "ERROR: The ${PROJECT} does not started!"
+	  else
+	     echo "Stopping the ${PROJECT} ..."
+	     for PID in ${PIDS};do
+	        kill ${PID} > /dev/null 2>&1
+	     done
+	     sleep 10
+	     PIDSA=`ps -ef | grep java | grep "${CONF_DIR}"|grep -v grep |awk '{print $2}'`
+	     if [ -n "${PIDSA}" ];then
+		for PID in ${PIDSA} ; do
+		   kill -9 ${PIDA} > /dev/null 2>&1
+		 done
+		   if [ -n "${PIDSA}" ];then
+		      echo "${PROJECT} stop err!"
+		   fi
+	     fi
+	  fi
+}
+
+STATUS () {
+	PIDS=`ps -ef | grep java | grep "${CONF_DIR}"|grep -v grep |awk '{print $2}'`
+	  if [ -z "${PIDS}" ]; then
+	     echo "${PROJECT} does not started!"
+	  else
+	     echo "${PROJECT} is running!"
+	  fi
+}
+
+
+case "$1" in
+	start|START)
+	START
+	;;
+	stop|STOP)
+	STOP
+	;;
+	restart|RESTART)
+	STOP
+	sleep 5
+	START
+	;;
+	status|STATUS)
+	STATUS
+	;;
+	*)
+	echo 'Please input start|stop|status|restart'
+	;;
+esac
