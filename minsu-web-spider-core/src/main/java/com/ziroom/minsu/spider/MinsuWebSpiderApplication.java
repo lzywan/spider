8076f8db1b9f3b66acb7afd67bd104e3981aae28
591629344@qq.com <068411Lsp>
2018-01-21 17:44:45 +0800

    测试提交

--- minsu-web-spider-core/src/main/java/com/ziroom/minsu/spider/MinsuWebSpiderApplication.java
+++ minsu-web-spider-core/src/main/java/com/ziroom/minsu/spider/MinsuWebSpiderApplication.java
@@ -2,7 +2,7 @@ package com.ziroom.minsu.spider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//lusp
 @SpringBootApplication
 public class MinsuWebSpiderApplication {
 
